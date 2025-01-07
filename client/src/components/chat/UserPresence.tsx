8f3dfe6aa4c657713903c8abb9e7b0de7d1a3fb3
koryk <380102-koryk@users.noreply.replit.com>
2025-01-07 23:41:33 +0000

    Agent query: Is the dropdown menu now visible when clicking the avatar in the top right?
    Refactor UserPresence component to use button for dropdown trigger and add styling.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/e09f1d61-6079-47e6-a03c-a3623ebd1c44/442c9a39-abd4-40f3-8ff9-1521fb911902.jpg

--- client/src/components/chat/UserPresence.tsx
+++ client/src/components/chat/UserPresence.tsx
@@ -21,19 +21,21 @@ export default function UserPresence() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className="focus:outline-none">
-        <Avatar>
-          <AvatarImage src={avatarUrl} />
-          <AvatarFallback>{user.username[0].toUpperCase()}</AvatarFallback>
-        </Avatar>
+      <DropdownMenuTrigger asChild>
+        <button className="relative flex h-8 w-8 items-center justify-center rounded-full outline-none hover:opacity-80 focus:ring-2 focus:ring-slate-400 focus:ring-offset-2">
+          <Avatar>
+            <AvatarImage src={avatarUrl} />
+            <AvatarFallback>{user.username[0].toUpperCase()}</AvatarFallback>
+          </Avatar>
+        </button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setLocation("/profile")}>
+      <DropdownMenuContent align="end" className="w-56">
+        <DropdownMenuItem onClick={() => setLocation("/profile")} className="cursor-pointer">
           <User className="mr-2 h-4 w-4" />
           <span>View Profile</span>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => logout()}>
+        <DropdownMenuItem onClick={() => logout()} className="cursor-pointer">
           <LogOut className="mr-2 h-4 w-4" />
           <span>Logout</span>
         </DropdownMenuItem>
