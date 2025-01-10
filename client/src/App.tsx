b501e7f1d224ac3cb06be4132245bd8f367a9970
koryk <380102-koryk@users.noreply.replit.com>
2025-01-10 00:15:26 +0000

    Restored to '37eb2467722f4c2131bbdbc086551bab7d93ebfb'
    Replit-Restored-To:37eb2467722f4c2131bbdbc086551bab7d93ebfb

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { useUser } from "@/hooks/use-user";
-import { UnreadProvider } from "@/hooks/use-unread";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
 import ProfilePage from "./pages/ProfilePage";
@@ -41,10 +40,8 @@ function App() {
 export default function AppWrapper() {
   return (
     <QueryClientProvider client={queryClient}>
-      <UnreadProvider>
-        <App />
-        <Toaster />
-      </UnreadProvider>
+      <App />
+      <Toaster />
     </QueryClientProvider>
   );
 }
